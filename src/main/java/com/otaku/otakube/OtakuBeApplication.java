d549fb0ab4073448d5910cb27ef8d7d2ada68381
akfrdma0125 <nogain1215@gmail.com>
2023-09-28 16:46:33 +0900

    init

---
+++ src/main/java/com/otaku/otakube/OtakuBeApplication.java
@@ -0,0 +1,13 @@
+package com.otaku.otakube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtakuBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtakuBeApplication.class, args);
+    }
+
+}
