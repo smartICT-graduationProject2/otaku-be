44ac75d5d174afd4f2738004ac8dd89a4f86c833
akfrdma0125 <nogain1215@gmail.com>
2023-09-29 17:54:38 +0900

    feat(#3): 인증과정 상수 정의

---
+++ src/main/java/com/otaku/otakube/common/security/consts/AuthConst.java
@@ -0,0 +1,21 @@
+package com.otaku.otakube.common.security.consts;
+
+public interface AuthConst {
+    String AUTHORIZATION_HEADER = "Authorization";
+    String BEARER_PREFIX = "Bearer ";
+    String ROLE = "ROLE";
+    String ROLE_USER = "ROLE_USER";
+    String ROLE_ADMIN = "ROLE_ADMIN";
+    String ROLE_HOST = "ROLE_HOST";
+
+    String[] AUTH_WHITELIST = {
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/api-docs/**",
+            "/health-check",
+            "/favicon.ico",
+            "/auth/**",
+            "/"
+    };
+}
