3a4d71e6b13dcea74dc2a5b6ff9f72f90b9991cb
akfrdma0125 <nogain1215@gmail.com>
2023-10-02 21:09:01 +0900

    remove: SpringBootTest 어노테이션 제거

--- src/test/java/com/otaku/otakube/OtakuBeApplicationTests.java
+++ src/test/java/com/otaku/otakube/OtakuBeApplicationTests.java
@@ -1,9 +1,8 @@
 package com.otaku.otakube;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OtakuBeApplicationTests {
 
     @Test
