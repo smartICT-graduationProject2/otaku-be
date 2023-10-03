679a4378d9ab6f373079b0c092f8215dd1621e75
akfrdma0125 <nogain1215@gmail.com>
2023-10-03 21:14:42 +0900

    feat(#10): HostInspection 엔티티 정의

---
+++ src/main/java/com/otaku/otakube/entity/user/HostInspection.java
@@ -0,0 +1,45 @@
+package com.otaku.otakube.entity.user;
+
+import com.otaku.otakube.entity.common.ApprovalStatus;
+import com.otaku.otakube.entity.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "t_host_inspection")
+public class HostInspection extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long hostInspectionId;
+
+    @Column(columnDefinition = "VARCHAR(200)")
+    private String authUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(9)")
+    private ApprovalStatus status;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column
+    private LocalDateTime modifiedDate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public HostInspection(String authUrl, User user) {
+        this.authUrl = authUrl;
+        this.status = ApprovalStatus.RECEPTION;
+        this.user = user;
+    }
+
+}
