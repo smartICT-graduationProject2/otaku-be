4aefaefdc42ff7a16b1aaaee8df85830aa4c67a6
akfrdma0125 <nogain1215@gmail.com>
2023-10-06 13:10:44 +0900

    feat(#10): 이벤트 승인 엔티티 수정

--- src/main/java/com/otaku/otakube/entity/log/Admission.java
+++ src/main/java/com/otaku/otakube/entity/log/Admission.java
@@ -6,42 +6,46 @@ import com.otaku.otakube.entity.event.Event;
 import com.otaku.otakube.entity.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 import static jakarta.persistence.FetchType.LAZY;
 
+@Table(name = "t_admission")
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Authentication extends BaseTimeEntity {
+public class Admission extends BaseTimeEntity {
 
     @Id
+    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "authentication_id", updatable = false)
     private Long authenticationId;
 
-    @Column(name = "x_nickname")
+    @Column
     private String xNickname;
 
-    @Column(name = "x_id")
+    @Column
     private String xId;
 
+    @Column
     @Enumerated(EnumType.STRING)
     private ApprovalStatus status;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
-    private User user;
+    private User appliedUser;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "event_id")
     private Event event;
 
-    public Authentication(String xNickname, String xId) {
+    @Builder
+    public Admission(String xNickname, String xId, User appliedUser, Event event) {
         this.xNickname = xNickname;
         this.xId = xId;
+        this.appliedUser = appliedUser;
+        this.event = event;
     }
 }
