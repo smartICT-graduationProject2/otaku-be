8fff154e08de4d39735b96d90b72668cb4477866
jinwon <jinwon0988@naver.com>
2023-10-08 06:47:29 +0900

    fix(#11): dto 이름 변경에 맞게 쿼리 수정

--- src/main/java/com/otaku/otakube/repository/event/EventRepository.java
+++ src/main/java/com/otaku/otakube/repository/event/EventRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     //오늘의 이벤트 조회
-    @Query("select new com.otaku.otakube.dto.event.response.EventInquiryResponseDto" +
+    @Query("select new com.otaku.otakube.dto.event.response.EventFindResponseDto" +
             "(e.eventId, e.name, e.xNickname, e.xId, sj.name, sp.currentAmount, sp.status)" +
             " from Event e join e.subject sj" +
             " join e.support sp")
