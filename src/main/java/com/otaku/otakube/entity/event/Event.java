d706ff1732743cdf1997be53f1ab85af4e14fa37
akfrdma0125 <nogain1215@gmail.com>
2023-10-09 14:39:55 +0900

    feat(#10): Event 엔티티 수정

--- src/main/java/com/otaku/otakube/entity/event/Event.java
+++ src/main/java/com/otaku/otakube/entity/event/Event.java
@@ -1,6 +1,8 @@
 package com.otaku.otakube.entity.event;
 
 import com.otaku.otakube.entity.common.BaseTimeEntity;
+import com.otaku.otakube.entity.log.Approval;
+import com.otaku.otakube.entity.log.EventLog;
 import com.otaku.otakube.entity.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -9,6 +11,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Random;
 
 import static jakarta.persistence.FetchType.LAZY;
@@ -16,50 +19,56 @@ import static jakarta.persistence.FetchType.LAZY;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
+@Table(name = "t_event")
 public class Event extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "event_id", updatable = false)
+    @Column
     private Long eventId;
 
+    @Column
     private String name;
 
+    @Column
     private String address;
 
+    @Column
     private Integer code;
 
-    @Column(name = "featured_image")
+    @Column
     private String featuredImage;
 
+    @Column
     private String description;
 
-    @Column(name = "is_public")
+    @Column
     private Boolean isPublic;
 
-    @Column(name = "x_nickname")
+    @Column
     private String xNickname;
 
-    @Column(name = "x_id")
+    @Column
     private String xId;
 
-    @Column(name = "perks_image")
+    @Column
     private String perksImage;
 
     @Temporal(TemporalType.DATE)
-    @Column(name = "opened_date")
+    @Column
     private LocalDate openedDate;
 
     @Temporal(TemporalType.DATE)
-    @Column(name = "closed_date")
+    @Column
     private LocalDate closedDate;
 
+    @Column
     @Enumerated(EnumType.STRING)
     private EventStatus status;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
-    private User user;
+    private User hostUser;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "subject_id")
@@ -68,30 +77,28 @@ public class Event extends BaseTimeEntity {
     @OneToOne(mappedBy = "event", fetch = LAZY)
     private Support support;
 
-    @Builder
-    public Event(String name, String address, String featuredImage, String description, Boolean isPublic, String xNickname, String xId, String perksImage, LocalDate openedDate, LocalDate closedDate, User user, Subject subject) {
-        this.name = name;
-        this.address = address;
-        this.featuredImage = featuredImage;
-        this.description = description;
-        this.isPublic = isPublic;
-        if (isPublic) {
-            Random random = new Random();
-            random.setSeed(System.currentTimeMillis());
-            this.code = random.nextInt(10000);
-        }
-        this.xNickname = xNickname;
-        this.xId = xId;
-        this.perksImage = perksImage;
-        this.openedDate = openedDate;
-        this.closedDate = closedDate;
-        this.status = EventStatus.PREPARATION;
-        this.user = user;
-        this.subject = subject;
-//        user.getEvents().add(this); //User의 events에 event 추가
-    }
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EventLog> eventLogs;
 
-    public void addSupport(Support support) {
-        this.support = support;
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Approval> approvalList;
+
+    @Builder
+    public Event(String name, String address, Integer code, String featuredImage, String description, Boolean isPublic, String xNickname, String xId, String perksImage, LocalDate openedDate, LocalDate closedDate, EventStatus status, User hostUser, Subject subject, Support support) {
+        this.name = name; //이벤트 이름
+        this.address = address; //이벤트 장소
+        this.code = code; //입장코드
+        this.featuredImage = featuredImage; //대표 이미지
+        this.description = description; //설명
+        this.isPublic = isPublic; //공개여부
+        this.xNickname = xNickname; //트위터 닉네임
+        this.xId = xId; //트위터 아이디
+        this.perksImage = perksImage; //특전 이미지
+        this.openedDate = openedDate; //시작
+        this.closedDate = closedDate; //마감
+        this.status = status; //상태
+        this.hostUser = hostUser; //호스트
+        this.subject = subject; //대상
+        this.support = support; //서포트, 없으면 널값
     }
 }
