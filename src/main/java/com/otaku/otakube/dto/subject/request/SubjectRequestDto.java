2b1c98fd25410f6f8dd2b5e4d58ee02b499d0a24
akfrdma0125 <nogain1215@gmail.com>
2023-10-10 13:49:02 +0900

    docs(#16): dto에 swagger 어노테이션 추가

--- src/main/java/com/otaku/otakube/dto/subject/request/SubjectRequestDto.java
+++ src/main/java/com/otaku/otakube/dto/subject/request/SubjectRequestDto.java
@@ -1,9 +1,17 @@
 package com.otaku.otakube.dto.subject.request;
 
 import com.otaku.otakube.entity.event.Subject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
+@Schema(name = "대상 등록 dto")
 public record SubjectRequestDto (
+        @Schema(description = "카테고리", defaultValue = "아이돌", example = "아이돌")
+        @NotBlank(message = "카테고리는 필수 입력값입니다.")
         String category,
+
+        @Schema(description = "대상 이름", defaultValue = "엑소 디오", example = "엑소 디오")
+        @NotBlank(message = "대상 이름은 필수 입력값입니다.")
         String subjectName
 ){
     public Subject toEntity(){
