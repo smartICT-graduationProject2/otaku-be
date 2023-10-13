21a2b9c54627df7c7fa49981e5e1d839cc5449ba
akfrdma0125 <nogain1215@gmail.com>
2023-10-13 10:51:50 +0900

    feat(#20): EventSaveRequestDto 유효성 및 설명 추가

--- src/main/java/com/otaku/otakube/dto/event/request/EventSaveRequestDto.java
+++ src/main/java/com/otaku/otakube/dto/event/request/EventSaveRequestDto.java
@@ -1,22 +1,45 @@
 package com.otaku.otakube.dto.event.request;
 
 import com.otaku.otakube.entity.event.Event;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+
 
 /**
  * 이벤트 등록 Request
  */
 public record EventSaveRequestDto(
-    Boolean isPublic,
-    String xNickname,
-    String xId,
-    String name, //이벤트 이름
-    Long subjectId, //대상 아이디
-    LocalDate openedDate,
-    LocalDate closedDate,
-    String address,
-    String description
+
+        @Schema(description = "이벤트 공개 여부")
+        @NotNull
+        Boolean isPublic,
+        @Schema(description = "트위터 닉네임")
+        @Size(min = 1, max = 100)
+        String xNickname,
+        @Schema(description = "트위터 아이디")
+        @Size(min = 1, max = 100)
+        String xId,
+        @Schema(description = "이벤트 이름")
+        @Size(min = 1, max = 100)
+        String name,
+        @Schema(description = "이벤트 대상 아이디")
+        @NotNull
+        Long subjectId,
+        @Schema(description = "이벤트 시작일")
+        @NotNull
+        LocalDate openedDate,
+        @Schema(description = "이벤트 마감일")
+        @NotNull
+        LocalDate closedDate,
+        @Schema(description = "이벤트 장소 주소")
+        @Size(min = 1, max = 255)
+        String address,
+        @Schema(description = "이벤트 설명")
+        @Size(min = 1, max = 500)
+        String description
 ){
     public Event toEntity(){
         return Event.builder()
