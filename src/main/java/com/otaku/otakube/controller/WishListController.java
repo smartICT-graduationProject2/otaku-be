4a6fb9d80b587bd478c4a94158eab368d6c9eabe
akfrdma0125 <nogain1215@gmail.com>
2023-10-22 13:15:59 +0900

    feat(#11): 이벤트 위시 리스트 삭제 기능 구현

---
+++ src/main/java/com/otaku/otakube/controller/WishListController.java
@@ -0,0 +1,72 @@
+package com.otaku.otakube.controller;
+
+import com.otaku.otakube.common.dto.response.BaseErrorResponseDto;
+import com.otaku.otakube.common.dto.response.BaseResponseDto;
+import com.otaku.otakube.service.event.WishListCreateService;
+import com.otaku.otakube.service.event.WishListDeleteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "WishList", description = "관심 이벤트 관련 API")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/wish-list")
+@Validated
+public class WishListController {
+
+    private final WishListCreateService wishListCreateService;
+    private final WishListDeleteService wishListDeleteService;
+
+    @Operation(summary = "관심 이벤트 등록 API", description = "관심 이벤트 등록 API입니다.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "생성 성공",
+                            useReturnTypeSchema = true
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "요청 실패",
+                            content = @Content(schema = @Schema(implementation = BaseErrorResponseDto.class))
+                    )
+            }
+    )
+    @PostMapping("/enroll")
+    public ResponseEntity<BaseResponseDto> enrollEventToWishList(
+            @Parameter @RequestParam final Long eventId) {
+        wishListCreateService.CreateWishList(eventId);
+        return BaseResponseDto.created();
+    }
+
+    @Operation(summary = "사용자의 후원 등록 API", description = "사용자가 특정 이벤트에 대해 후원하는 API입니다.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "등록 성공",
+                            useReturnTypeSchema = true
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "요청 실패",
+                            content = @Content(schema = @Schema(implementation = BaseErrorResponseDto.class))
+                    )
+            }
+    )
+    @DeleteMapping(value = "/cancel")
+    public ResponseEntity<BaseResponseDto> deleteWishList(
+            @Parameter @RequestParam final Long eventId) {
+        wishListDeleteService.DeleteWishList(eventId);
+        return BaseResponseDto.created();
+    }
+}
--- src/main/java/com/otaku/otakube/repository/log/WishListRepository.java
+++ src/main/java/com/otaku/otakube/repository/log/WishListRepository.java
@@ -2,14 +2,12 @@ package com.otaku.otakube.repository.log;
 
 import com.otaku.otakube.entity.log.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface WishListRepository extends JpaRepository<WishList, Long> {
-
-    //userId 관심 이벤트 조회
-    @Query("select w.event.eventId from WishList w where w.user.userId = :userId")
-    List<Long> findWishEvents(@Param("userId") Long userId);
+    @Modifying
+    @Query("delete from WishList w where w.event.eventId = :eventId AND w.user.userId = :userId")
+    void deleteWishListByEventAndUser(@Param("eventId") final Long eventId, @Param("userId") final Long userId);
 }
