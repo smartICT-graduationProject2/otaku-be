68ffad13ec4f92a2975d62be3c22f5a7863726da
akfrdma0125 <nogain1215@gmail.com>
2023-10-22 13:15:35 +0900

    feat(#11): 이벤트 위시 리스트 추가 기능 구현

--- src/main/java/com/otaku/otakube/service/event/EventReadService.java
+++ src/main/java/com/otaku/otakube/service/event/EventReadService.java
@@ -3,26 +3,31 @@ package com.otaku.otakube.service.event;
 import com.otaku.otakube.common.exception.constants.ErrorDetails;
 import com.otaku.otakube.common.exception.custom.event.EventException;
 import com.otaku.otakube.entity.event.Event;
+import com.otaku.otakube.entity.event.EventStatus;
 import com.otaku.otakube.repository.event.EventRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Slf4j
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class EventReadService {
 
     private final EventRepository eventRepository;
 
-    /**
-     * 이벤트 등록
-     */
-    @Transactional
+    @Transactional(readOnly = true)
     public Event findEventById(final Long eventId) {
         return eventRepository.findById(eventId)
+                .orElseThrow(() -> EventException.of(ErrorDetails.EVENT_NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true)
+    public Event findEventByIdAndStatus(final Long eventId) {
+        return eventRepository.findByEventIdAndStatusNotIn(eventId, List.of(EventStatus.DELETED, EventStatus.CLOSED))
                 .orElseThrow(() -> EventException.of(ErrorDetails.EVENT_NOT_FOUND));
     }
 }
