88fa5e91a29604ff23c6b7f8082181c4601d14d4
akfrdma0125 <nogain1215@gmail.com>
2023-10-25 16:05:21 +0900

    fix(#19): merge conflict

--- src/main/java/com/otaku/otakube/entity/event/Event.java
+++ src/main/java/com/otaku/otakube/entity/event/Event.java
@@ -3,6 +3,7 @@ package com.otaku.otakube.entity.event;
 import com.otaku.otakube.entity.common.BaseTimeEntity;
 import com.otaku.otakube.entity.log.Approval;
 import com.otaku.otakube.entity.log.EventLog;
+import com.otaku.otakube.entity.log.WishList;
 import com.otaku.otakube.entity.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -11,6 +12,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static jakarta.persistence.FetchType.LAZY;
@@ -76,28 +78,44 @@ public class Event extends BaseTimeEntity {
     @OneToOne(mappedBy = "event", fetch = LAZY)
     private Support support;
 
-    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<EventLog> eventLogs;
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true, fetch = LAZY)
+    private List<Approval> approvalList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Approval> approvalList;
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true, fetch = LAZY)
+    private List<WishList> wishLists = new ArrayList<>();
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true, fetch = LAZY)
+    private List<EventLog> eventLogs = new ArrayList<>();
+
+    public void saveAdditionalEventInformation(final User hostUser, final Subject eventSubject, final Integer code){
+        this.hostUser = hostUser;
+        this.subject = eventSubject;
+        this.code = code;
+    }
+
+    public void saveImageInformation(final String perksImageUrl, final String featuredImageUrl){
+        this.perksImage = perksImageUrl;
+        this.featuredImage = featuredImageUrl;
+    }
+
+    public void completeEventSupport(){
+        this.status = EventStatus.PREPARATION;
+    }
+
+    public void registerEventSupport(){
+        this.status = EventStatus.UNDEFINED;
+    }
 
     @Builder
-    public Event(String name, String address, Integer code, String featuredImage, String description, Boolean isPublic, String xNickname, String xId, String perksImage, LocalDate openedDate, LocalDate closedDate, EventStatus status, User hostUser, Subject subject, Support support) {
+    public Event(String name, String address, String description, Boolean isPublic, String xNickname, String xId, LocalDate openedDate, LocalDate closedDate) {
         this.name = name; //이벤트 이름
         this.address = address; //이벤트 장소
-        this.code = code; //입장코드
-        this.featuredImage = featuredImage; //대표 이미지
         this.description = description; //설명
-        this.isPublic = isPublic; //공개여부
+        this.isPublic = isPublic; //공개 여부
         this.xNickname = xNickname; //트위터 닉네임
         this.xId = xId; //트위터 아이디
-        this.perksImage = perksImage; //특전 이미지
         this.openedDate = openedDate; //시작
         this.closedDate = closedDate; //마감
-        this.status = status; //상태
-        this.hostUser = hostUser; //호스트
-        this.subject = subject; //대상
-        this.support = support; //서포트, 없으면 널값
+        this.status = EventStatus.PREPARATION;
     }
 }
